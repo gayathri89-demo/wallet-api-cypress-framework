64fbc2e0481a18601bd41169e41890dfaa467be2
Gayathri <gayathri.r.nair89@gmail.com>
2026-06-05 16:23:48 +0400

    Add environment variable validation

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,14 @@
 const { defineConfig } = require("cypress");
 require("dotenv").config();
 
+const requiredEnvVars = ["BASE_URL", "USERNAME", "PASSWORD", "SERVICE_ID"];
+
+requiredEnvVars.forEach((key) => {
+  if (!process.env[key]) {
+    throw new Error(`Missing required environment variable: ${key}`);
+  }
+});
+
 module.exports = defineConfig({
   e2e: {
     supportFile: "cypress/support/e2e.js",
